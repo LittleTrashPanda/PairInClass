d72e569b3bfc70f11e0f37375ee7123779577b5b
LittleTrashPanda <trueapartment13@gmail.com>
2026-02-06 14:46:01 -0500

    Added line to main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("ARGHHHHHHHH!");
+        System.out.println("ARGH!");
+
+        System.out.println("Blegh");
     }
 }
